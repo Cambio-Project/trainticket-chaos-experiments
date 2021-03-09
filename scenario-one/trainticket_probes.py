25b8fbb9af7804c4f7e98c79f9557fdbc527a300
Dominik Huber <elcpt@elcpt.de>
2021-03-09 16:27:51 +0100

    Initial (client) commit

---
+++ scenario-one/trainticket_probes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# Big burger or oily bean curd, that is the question
+def hello_world(echo):
+    print(f"Hello world from the TrainTicket hello world probe. Echo is: {echo}")
+    return True
