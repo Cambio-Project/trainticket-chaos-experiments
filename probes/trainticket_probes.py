43d4b3cea5f738d4596dc68057af91758faebab6
Marvin Munoz Baron <st141535@stud.uni-stuttgart.de>
2021-04-22 22:38:02 +0200

    Update chaos experiment and probes for scenario 3

--- probes/trainticket_probes.py
+++ probes/trainticket_probes.py
@@ -196,3 +196,108 @@ def scenario_two_broken_probe():
     print("The order has been paid, indicating that the fault injection failed.")
     kill_load_generators()
     return False
+    
+
+def scenario_three_steady_state_probe():
+    # TODO: Move this login/auth part into a new function to avoid duplicate code
+    # Start the Load Generator to create background load reflecting user interaction with the system
+    os.system(f"{os.getcwd()}/steady-state.sh")
+    print("Loadgenerators started, beginning steady state check")
+
+    # Log in to TrainTicket to be authorized for subsequent requests
+    login_response = requests.post(f"http://localhost:8080/api/v1/users/login", json = {"username": "admin", "password": "222222"})
+    login_response_parsed = login_response.json()
+    auth_header = {'Authorization': f"Bearer {login_response_parsed['data']['token']}"}
+    user_id = login_response_parsed['data']['userId']
+
+    # Get a contact id, we need that for reservations
+    contacts_response = requests.get("http://localhost:12347/api/v1/contactservice/contacts", headers=auth_header)
+    contact = contacts_response.json()["data"][0]
+    contact_id = contact["id"]
+    account_id = contact["accountId"]
+
+    # TODO: Move ticket reservation part into a new function to avoid duplicate code
+    # Reserve a ticket, we do however not get the order ID back from the reservation ("preserve") service
+    trip_id = "D1345"
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    reserve_json = {
+        "accountId": account_id,
+        "contactsId": contact_id,
+        "tripId":trip_id,
+        "seatType":"2",
+        "date": tomorrow.strftime("%Y-%m-%d"),
+        "from":"Shang Hai",
+        "to":"Su Zhou",
+        "assurance":"0",
+        "foodType":1,
+        "foodName":"Bone Soup",
+        "foodPrice":2.5,
+        "stationName":"",
+        "storeName":""
+    }
+    reserve_response = requests.post("http://localhost:14568/api/v1/preserveservice/preserve", json=reserve_json, headers=auth_header)
+    #print(f"Reservation response: {reserve_response.json()}")
+    if not reserve_response.json()["status"] == 1:
+        print("Reservation failed, failing this probe.")
+        kill_load_generators()
+        return False
+
+    print("Everything successful, ending experiment with success.")
+    kill_load_generators()
+    return True
+
+
+def scenario_three_switch_faulty_preserve_controller():
+    print("Switching database configs to bad one")
+    os.system(f"{os.getcwd()}/switch-preserve-controller.sh")
+
+
+def scenario_three_rollback_preserve_controller():
+    print("Switching back to good config")
+    os.system(f"{os.getcwd()}/rollback-preserve-controller.sh")
+
+def scenario_three_broken_probe():
+    # Start the Load Generator to create background load reflecting user interaction with the system
+    os.system(f"{os.getcwd()}/steady-state.sh")
+    print("Loadgenerators started, beginning steady state check")
+
+    # Log in to TrainTicket to be authorized for subsequent requests
+    login_response = requests.post(f"http://localhost:8080/api/v1/users/login", json = {"username": "admin", "password": "222222"})
+    login_response_parsed = login_response.json()
+    auth_header = {'Authorization': f"Bearer {login_response_parsed['data']['token']}"}
+    user_id = login_response_parsed['data']['userId']
+
+    # Get a contact id, we need that for reservations
+    contacts_response = requests.get("http://localhost:12347/api/v1/contactservice/contacts", headers=auth_header)
+    contact = contacts_response.json()["data"][0]
+    contact_id = contact["id"]
+    account_id = contact["accountId"]
+
+    # Reserve a ticket, we do however not get the order ID back from the reservation ("preserve") service
+    trip_id = "D1345"
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    reserve_json = {
+        "accountId": account_id,
+        "contactsId": contact_id,
+        "tripId":trip_id,
+        "seatType":"2",
+        "date": tomorrow.strftime("%Y-%m-%d"),
+        "from":"Shang Hai",
+        "to":"Su Zhou",
+        "assurance":"0",
+        "foodType":1,
+        "foodName":"Bone Soup",
+        "foodPrice":2.5,
+        "stationName":"",
+        "storeName":""
+    }
+    reserve_response = requests.post("http://localhost:14568/api/v1/preserveservice/preserve", json=reserve_json, headers=auth_header)
+    #print(f"Reservation response: {reserve_response.json()}")
+    if reserve_response.headers['Content-Type'] != "application/json":
+        print("Response type is not JSON - fault is happening.")
+        kill_load_generators()
+        return True
+
+    print("The response type is still JSON, indicating that the fault injection failed.")
+    kill_load_generators()
+    return False
