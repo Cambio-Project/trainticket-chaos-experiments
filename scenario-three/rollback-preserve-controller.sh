bd53aca418e3c4f6a99b4e0ecd8732cdb0aad2f2
Marvin Munoz Baron <st141535@stud.uni-stuttgart.de>
2021-04-26 18:16:57 +0200

    Rename Preserve Controller Alternatives

--- scenario-three/rollback-preserve-controller.sh
+++ scenario-three/rollback-preserve-controller.sh
@@ -1,8 +1,8 @@
 # shellcheck disable=SC2164
 cd ../../trainticket-fork/ts-preserve-service/src/main/java/preserve/controller
 rm PreserveController.java
-cp PreserveController.SAFE.java PreserveController.java
-cd ../../../../../../trainticket-fork/deployment/docker-compose-manifests/
+cp PreserveController.SAFE PreserveController.java
+cd ../../../../../../deployment/docker-compose-manifests/
 sudo docker-compose -f docker-compose-with-jaeger.yml stop ts-preserve-service
 sudo docker-compose -f docker-compose-with-jaeger.yml rm -f ts-preserve-service
 sudo docker-compose -f docker-compose-with-jaeger.yml up --build --no-deps --force-recreate -d ts-preserve-service
