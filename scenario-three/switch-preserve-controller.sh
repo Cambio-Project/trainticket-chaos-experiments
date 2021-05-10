8c2b525ae89b3038194cf07acb688ffde286f61c
Marvin Munoz Baron <st141535@stud.uni-stuttgart.de>
2021-05-10 12:08:20 +0200

    Change scenario 3 to replace target JAR

--- scenario-three/switch-preserve-controller.sh
+++ scenario-three/switch-preserve-controller.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2164
-cd ../../trainticket-fork/ts-preserve-service/src/main/java/preserve/controller
-rm PreserveController.java
-cp PreserveController.FAULTY PreserveController.java
+cd ../../trainticket-fork/ts-preserve-service/target
+rm ts-preserve-service-1.0.jar
+cp ts-preserve-service-1.0.FAULTY ts-preserve-service-1.0.jar
 cd ../../../../../../deployment/docker-compose-manifests/
 sudo docker-compose -f docker-compose-with-jaeger.yml stop ts-preserve-service
 sudo docker-compose -f docker-compose-with-jaeger.yml rm -f ts-preserve-service
